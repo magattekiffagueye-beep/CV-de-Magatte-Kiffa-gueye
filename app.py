72372fbccea17e74e43c99050f0b81974dcb475d
magattekiffagueye-beep <magattekiffagueye@gmail.com>
2026-05-08 21:59:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -37,8 +37,9 @@ Email: magattekiffagueye@gmail.com
 
 
  Adresse: Saint Louis, Sénegal""")
+      st.write("Je reste entièrement disponible pour tout complément d’information et pour un éventuel entretien à votre convenance."
 
-st.write("Je reste entièrement disponible pour tout complément d’information et pour un éventuel entretien à votre convenance.")
+
     
 
 
