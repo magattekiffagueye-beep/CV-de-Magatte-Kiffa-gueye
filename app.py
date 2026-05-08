fa06e25a9644b28f365d859bb88444f77e3b66cb
magattekiffagueye-beep <magattekiffagueye@gmail.com>
2026-05-08 11:38:06 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st 
+
+
+
+st.title("MAGATTE KIFFA GUEYE")
+st.header("Etudiante en science agricole et alimentaire")
+st.markdown("""          
+Je suis étudiante en sciences agricoles, alimentaires et nutritionnelles. Je maîtrise les bases de l’analyse en sciences agroalimentaires et les techniques de laboratoire simples, ainsi que les méthodes de collecte et d’interprétation de données.""")
+st.subheader("""FORMATION
+2025 – 2026 : Deuxième année en Sciences Agricoles et Alimentaires
+
+2024 – 2025 : Première année en Sciences Agricoles et Alimentaires
+                                                                                                                                                     
+2023 – 2024 : Baccalauréat – Lycée de Jeune Ameth Fall""")
+st.subheader("""EXPERIENCE PROFESSINONNELLE
+Visites pédagogiques : observation des cultures et techniques agricoles
+
+Participation aux activités d’entretien de champ
+
+Travaux d’irrigation et activités agricoles pratiques
+
+Participation à des activités de laboratoire de base""")
+
+st.subheader("""COMPETENCE TECHNIQUE
+Observation des cultures et travaux agricoles
+
+Observation des cultures et travaux agricoles
+
+Notions de production agroalimentaire""")
+
+
+with st.sidebar:
+    st.write("cc")
+    
+    
+
+
+
+
+
+
