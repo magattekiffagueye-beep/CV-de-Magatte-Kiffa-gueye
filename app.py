f2951c1ad2e1d4d6d3c593722949e28ed9cfba06
magattekiffagueye-beep <magattekiffagueye@gmail.com>
2026-05-09 10:27:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 st.title("MAGATTE KIFFA GUEYE")
-st.header("Technicienne Supérieure en science agricole et alimentaire")
+st.header("Technicienne supérieure en science agricole et alimentaire")
 st.markdown("""          
 Je suis étudiante en sciences agricoles, alimentaires et nutritionnelles. Je maîtrise les bases de l’analyse en sciences agroalimentaires et les techniques de laboratoire simples, ainsi que les méthodes de collecte et d’interprétation de données.""")
 st.subheader("""FORMATION
@@ -11,7 +11,7 @@ st.subheader("""FORMATION
 
 
                                                                                                                                                      
-2023 – 2024 : Diplome de Baccalauréat """)
+2023 – 2024 : diplome de baccalauréat)
 st.subheader("""EXPERIENCE PROFESSINONNELLE
 Visites pédagogiques : observation des cultures et techniques agricoles
 
@@ -26,22 +26,16 @@ Observation des cultures et travaux agricoles
 
 Observation des cultures et travaux agricoles
 
-Notions de production agroalimentaire
-
-Compétences en suite numérique""")
+Notions de production agroalimentaire""")
 
 
 with st.sidebar:
-    st.subheader("""CONTACT 
-Email: magattekiffagueye@gmail.com
+    st.subheader("""CONTACTS
+
+    Email: magattekiffagueye@gmail.com
 
 
- Adresse: Saint Louis, Sénegal""")
-  with st.sidebar:
-    st.write("Je reste entièrement disponible pour tout complément d’information et pour un éventuel entretien à votre convenance.")
     
-
-
     
 
 
