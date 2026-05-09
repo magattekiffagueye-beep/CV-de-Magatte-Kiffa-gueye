a1dd1ec2fa512bfa13c460b1c68bfa363f9d73bb
magattekiffagueye-beep <magattekiffagueye@gmail.com>
2026-05-09 23:21:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,25 @@ import streamlit as st
 
 
 st.title("MAGATTE KIFFA GUEYE")
-st.header("Technicienne supérieure en science agricole et alimentaire")
+st.header("Etudiante en science agricole et alimentaire")
 st.markdown("""          
 Je suis étudiante en sciences agricoles, alimentaires et nutritionnelles. Je maîtrise les bases de l’analyse en sciences agroalimentaires et les techniques de laboratoire simples, ainsi que les méthodes de collecte et d’interprétation de données.""")
 st.subheader("""FORMATION
-2025 – 2026 : Technicienne supérieure en Sciences Agricoles et Alimentaires
+2025 – 2026 : Deuxième année en Sciences Agricoles et Alimentaires
 
 
                                                                                                                                                      
-2023 – 2024 : diplome de baccalauréat)
-st.subheader("""EXPERIENCE PROFESSINONNELLE
+2023 – 2024 : Baccalauréat – Lycée de Jeune Amethl""")
+st.subheader("""EXPERIENCES PROFESSINONNELLES
 Visites pédagogiques : observation des cultures et techniques agricoles
 
-Participation aux activités d’entretien de champs
+Participation aux activités d’entretien de champ
 
 Travaux d’irrigation et activités agricoles pratiques
 
 Participation à des activités de laboratoire de base""")
 
-st.subheader("""COMPETENCE TECHNIQUE
+st.subheader("""COMPETENCE TECHNIQUES
 Observation des cultures et travaux agricoles
 
 Observation des cultures et travaux agricoles
@@ -30,9 +30,38 @@ Notions de production agroalimentaire""")
 
 
 with st.sidebar:
-    st.subheader("""CONTACTS
 
-    Email: magattekiffagueye@gmail.com""")
+
+
+
+    
+    st.image("photo.jpeg",width=150)
+    
+    
+    
+    
+
+    
+    st.subheader("CONTACTS")
+
+    
+    st.write("email: gueye.kiffa@uam.edu.sn")  
+    st.write("adress:Saint Louis, Sénégal")
+    st.markdown("""
+
+
+
+   
+                                               je reste disponible pour tout complement d'information et pour un éventuel entretien à votre convenance.""")
+st.subheader("""PROJETS REALISES
+Integration dans le club environnementale ECO HUMAN en tant que secrétaire génerale en 2025
+
+Etant une couturiére:Lancement de mon activité de couture avec commercialisation en ligne 
+
+Rédiger des Comptes rendus de reunion et Faire des présentations à partir des logiciels Word, Excel et PowerPoint """)
+st.image("Photo champ.jpeg",width=300)
+st.video("BCD.mp4",width=300)
+
 
 
     
