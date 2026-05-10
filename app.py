0d2a57cd997c64c9c02661b01134219e5d2036ab
magattekiffagueye-beep <magattekiffagueye@gmail.com>
2026-05-10 16:20:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ Rédiger des Comptes rendus de reunion et Faire des présentations à partir des
 
 
 
-st.image("photo champs.jpeg",width=300)
+st.image("photo champ.jpeg",width=300)
 
 st.video("BCD.mp4",width=300)
 
